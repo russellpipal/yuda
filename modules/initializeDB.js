86c28463d2486dfb39b83c46cb3dcfc99f44da83
russellpipal <russell.pipal@gmail.com>
2016-06-13 17:31:30 -0500

    fleshes out initializeDB, queries needed

--- modules/initializeDB.js
+++ modules/initializeDB.js
@@ -1,5 +1,48 @@
 var pg = require('pg');
 
+var connectionString;
+
+if (process.env.DATABASE_URL){
+  pg.defaults.ssl = true;
+  console.log('environment var');
+  connectionString = process.env.DATABASE_URL;
+} else {
+  console.log('local var');
+  connectionString = "postgres://localhost:5432/yuda";
+}
+
+function initializeDB(){
+  return new Promise(function(resolve, reject) {
+    pg.connect(connectionString, function(err, client, done){
+      console.log('connected to pg');
+      if(err){
+        console.log('Error connecting to DB!', err);
+        reject(Error(err));
+        process.exit(1);
+
+      } else {
+
+        var userTable, goalTable, friendTable;
+        // add queries here
+        var query = client.query(userTable + goalTable + friendTable);
 
 
-module.exports = initializeDB;
+        query.on('end', function(){
+          console.log('tables created');
+          resolve();
+          done();
+        });
+
+
+        query.on('error', function(err) {
+          console.log('Error executing query', err);
+          reject(Error(err));
+        });
+      }
+
+    });
+  });
+}
+
+module.exports.connectionString = connectionString;
+module.exports.initializeDB = initializeDB;
